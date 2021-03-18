119955d711397d0266ff0280f4f51c5b12e03ee3
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2021-03-17 22:04:04 -0300

    docs: configure swagger on project

---
+++ src/main/java/com/luisbilecki/openfeignexample/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.luisbilecki.openfeignexample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.luisbilecki.openfeignexample.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+        private ApiInfo apiInfo() {
+            return new ApiInfo(
+                    "Rick and Morty API",
+                    "See characters of Ricky and Morty.",
+                    "1.0.0",
+                    "",
+                    new Contact("Luís Felipe BIlecki", "", ""),
+                    "", "", Collections.emptyList());
+    }
+
+}
